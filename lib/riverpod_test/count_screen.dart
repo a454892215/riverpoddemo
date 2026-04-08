0ee8c0bb133624cb77e6f5758ef572f28a34ab86
web <web@Macmini.localdomain>
2026-04-08 17:11:10 +0400

    yh 88

--- lib/riverpod_test/count_screen.dart
+++ lib/riverpod_test/count_screen.dart
@@ -9,8 +9,10 @@ class CounterScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    /// 【重点 1: ref.watch】 —— 用于监听（具体是监听counterProvider中的或者对应的什么？）
-    /// 只要 counter 的值变了（这是什么意思 Count类的值是指什么），build 方法就会重新运行，UI 自动刷新。
+    /// 【重点 1: ref.watch】 —— 用于监听目标对象对应的使用了@riverpod修饰的类的build()方法返回的对象
+    /// 只要监听的对象改变了 就会触发 该ConsumerWidget的Widget build(BuildContext context, WidgetRef ref) 重新调用。
+    ///  问题1： 如果监听的是一个一个自定义类的对象，这个对象的某个属性改变了，也会触发更新吗？
+    ///  问题2： 如果监听的是一个List呢，List中的 某个对象的某个属性改变了，也会触发更新吗？
     final count = ref.watch(counterProvider);
 
     return Scaffold(
@@ -23,7 +25,8 @@ class CounterScreen extends ConsumerWidget {
             ElevatedButton(
               onPressed: () {
                 /// 【重点 2: ref.read】 —— 用于读取counterProvider对应的值，但是为什么counterProvider后面.notifier?
-                /// 只读取当前那一刻的对象，不监听后续变化。 就像 Get.find<Counter>().increment()
+                /// 1. ref.read(counterProvider)：读取的是 数据本身（那个 int 数字）。数字是没有 increment() 方法的，所以直接调用会报错
+                /// 2. ref.read(counterProvider.notifier)：读取的是 控制器对象实例（即 Counter 类的实例）
                 ref.read(counterProvider.notifier).increment();
               },
               child: const Text('加 1'),
@@ -31,7 +34,7 @@ class CounterScreen extends ConsumerWidget {
 
             ElevatedButton(
               onPressed: () {
-                ///
+                /// 这个刷新方法是什么意思？有什么作用？
                 ref.refresh(counterProvider);
               },
               child: const Text('ref.refresh 方法作用-调用测试'),
